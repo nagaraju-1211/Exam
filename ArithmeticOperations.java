067485ac16757c9c1697e27517655fcc0df5bf13
nagaraju <245122749001@mvsrec.edu.in>
2025-07-25 11:16:54 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Diff:" + (a-b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("division:" + (a/b));
+    }
+}
